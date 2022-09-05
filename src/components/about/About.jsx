98611d6f3cae7024fa1a3b8d95da6249a5789e06
Ojaswi <ojaswiawasthi2025@gmail.com>
2022-09-05 19:55:28 +0530

    duplicated content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './about.css'
 import AboutMe from '../../assets/about_me.jpg'
+import {FaAward} from 'react-icons/fa'
 
 const About = () => {
   return (
@@ -16,7 +17,26 @@ const About = () => {
         </div>
 
         <div className="about__content">
+            <div className="about__cards">
+              <article className='about__card'>
+                <FaAward />
+                <h5>Experience</h5>
+                <small>1+ year working</small>
+              </article>
 
+              <article className='about__card'>
+                <FaAward />
+                <h5>Company</h5>
+                <small>Paymentus Corporation</small>
+              </article>
+
+              <article className='about__card'>
+                <FaAward />
+                <h5>Projects</h5>
+                <small>5+ Completed Projects</small>
+              </article>
+
+            </div>
         </div>
 
       </div>
