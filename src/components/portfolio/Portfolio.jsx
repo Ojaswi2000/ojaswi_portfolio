faf2279eb42a966ac2f4065a6a9c8e92737c849e
Ojaswi <ojaswiawasthi2025@gmail.com>
2022-09-07 00:50:04 +0530

    done portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -68,11 +68,22 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <h3>React Chat App using Firebase </h3>
-          </div>
-        </article>
+        {
+          data.map(({id, image,title, github, demo}) => {
+            return(
+              <article key={id} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            );
+          })
+        }
       </div>
     </section>
   )
