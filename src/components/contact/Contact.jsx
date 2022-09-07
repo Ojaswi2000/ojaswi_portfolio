3b698d0ef5b7c76129fcb1bb30f04d51c79c8646
Ojaswi <ojaswiawasthi2025@gmail.com>
2022-09-07 18:35:04 +0530

    article email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,23 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <h4>Email</h4>
+            <h5>ojaswiawasthi2025@gmail.com</h5>
+            <a href="mailTo:ojaswiawasthi2025@gmail.com">Send a Message</a>
+          </article>
+        </div>
+
+        <form action="">
+
+        </form>
+      </div>
+    </section>
   )
 }
 
